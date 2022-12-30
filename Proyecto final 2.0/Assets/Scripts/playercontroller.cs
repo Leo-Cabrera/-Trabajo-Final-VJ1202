2bcf09bc0f95fd1ff33173eebf76a08830f90445
Leo-Cabrera <kanomody29@gmail.com>
2022-12-30 23:40:24 +0100

    animations 1/2

--- Proyecto final 2.0/Assets/Scripts/playercontroller.cs
+++ Proyecto final 2.0/Assets/Scripts/playercontroller.cs
@@ -4,18 +4,43 @@ using UnityEngine;
 
 public class playercontroller : MonoBehaviour
 {
-   // Start is called before the first frame update
-   void Start()
-   {
-   }
-   // Update is called once per frame
-   void Update()
-   {
-       float horizontal = Input.GetAxis("Horizontal");
-       float vertical = Input.GetAxis("Vertical");
-       Vector2 position = transform.position;
-       position.x = position.x + 3.0f * horizontal * Time.deltaTime;
-       position.y = position.y + 3.0f * vertical * Time.deltaTime;
-       transform.position = position;
-   }
+    public float runSpeed = 2;
+    public float jumpSpeed = 3;
+
+    Rigidbody2D rg2d;
+
+    void Start()
+    {
+        rg2d = GetComponent<Rigidbody2D>();
+
+    }
+    void FixedUpdate()
+    {
+
+        //right and left move
+
+        if(Input.GetKey("d")|| Input.GetKey("right"))
+        {
+            rg2d.velocity = new Vector2(runSpeed, rg2d.velocity.y);
+
+        }
+
+        else if(Input.GetKey("a")|| Input.GetKey("left"))
+        {
+            rg2d.velocity = new Vector2(-runSpeed, rg2d.velocity.y);
+            
+        }
+
+        else
+        {
+            rg2d.velocity = new Vector2 (0,rg2d.velocity.y);
+        }
+
+        // jump
+        if( Input.GetKey("w") && checkGround.isGrounded)
+        {
+            rg2d.velocity = new Vector2 (rg2d.velocity.x , jumpSpeed);
+        }
+       
+    }
 }
